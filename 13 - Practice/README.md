34c2200f28ae2b89dff5fcaf2d56f5d333b80420
ElizavetaBB <borisenkoli01@mail.ru>
2023-06-06 02:40:41 +0300

    End Section 13

---
+++ 13 - Practice/README.md
@@ -0,0 +1,2 @@
+# Notes
+Geocoding API от Yandex - https://yandex.ru/dev/maps/jsapi/doc/2.1/quick-start/index.html
---
+++ 13 - Practice/src/app.ts
@@ -0,0 +1,58 @@
+// Code goes here!
+import axios from 'axios';
+
+let form = document.querySelector("form")! as HTMLFormElement;
+let inputAddress = document.getElementById("address")! as HTMLInputElement;
+
+const YANDEX_API_KEY = 'a300a83c-b108-442a-898e-5530f1d4eaf5';
+declare let ymaps: any;
+
+let myMap: any;
+
+type YandexGeoCodingResponse = {
+    response: {
+        GeoObjectCollection: { 
+            featureMember : [{
+                GeoObject: { 
+                    Point : { pos : string}
+                }
+            }]
+        }
+    }
+}
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    let enteredAddress = inputAddress.value;
+    // send to Yandex API
+
+    axios.get<YandexGeoCodingResponse>(
+        `https://geocode-maps.yandex.ru/1.x/?apikey=${YANDEX_API_KEY}&geocode=${encodeURI(enteredAddress)}&format=json`
+    ).then(response => {
+        let coordinates = response.data
+            .response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos! as string;
+        let coordinatesSplitted = coordinates.split(' ');
+
+        if (myMap) {
+            myMap.destroy();
+        }
+        
+        myMap = new ymaps.Map(document.getElementById('map')!, {
+            center: [coordinatesSplitted[1], coordinatesSplitted[0]],
+            zoom: 15
+        });
+        
+        let myGeoObject = new ymaps.GeoObject({
+            geometry: {
+                type: "Point",
+                coordinates: [coordinatesSplitted[1], coordinatesSplitted[0]]
+            },
+        });
+
+        myMap.geoObjects.add(myGeoObject);
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+form.addEventListener('submit', searchAddressHandler);
