b2f078219a1f6b456c0d92a56c02daf69ccdd466
ElizavetaBB <borisenkoli01@mail.ru>
2023-06-06 23:43:31 +0300

    End Section 15

---
+++ 15 - NodeJS + Express/README.md
@@ -0,0 +1,12 @@
+# Notes
+## Created api
+Post method:
+![Screenshot](images/post_method.png)
+Get method:
+![Screenshot](images/get_method.png)
+Patch method:
+![Screenshot](images/patch_method.png)
+Delete method:
+![Screenshot](images/delete_method.png)
+## Documentation
+Build web applications/REST api with typescript (decorators, interfaces, etc.) - https://docs.nestjs.com/
---
+++ 15 - NodeJS + Express/src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { json } from 'body-parser';
+import todoRouter from './routes/todos';
+
+const app = express();
+
+app.use(json());
+
+app.use('/todos', todoRouter);
+
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(500).json({message: err.message});
+});
+
+app.listen(3000);
